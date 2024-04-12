3e781c3aa87b0bda72372cf81cfb661d622da8e9
Xu Yiming <xuyiming.open@outlook.com>
2024-04-13 02:03:39 +0800

    build: Fix node_modules exclusion

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ const config = {
             },
           },
         ],
-        exclude: ["/node_modules/"],
+        exclude: [/([/\\])node_modules\1/],
       },
       {
         test: /\.tsx$/i,
@@ -98,12 +98,12 @@ const config = {
             },
           },
         ],
-        exclude: ["/node_modules/"],
+        exclude: [/([/\\])node_modules\1/],
       },
       {
         test: /\.(jsx?)$/i,
         loader: "babel-loader",
-        exclude: ["/node_modules/"],
+        exclude: [/([/\\])node_modules\1/],
       },
       {
         test: /\.less$/i,
